59beef6d55a0b1555d086c2f2eabca9895efa07d
Nehal <jhalanehal@gmail.com>
2021-06-21 10:34:23 -0700

    Initial Commit

---
+++ Headache Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Headache Tracker
+//
+//  Created by Nehal Jhala on 6/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
